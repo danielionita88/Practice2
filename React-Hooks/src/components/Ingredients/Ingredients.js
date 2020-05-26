df5d1a0b5f6fefa26e692201cf185eee73470c18
Daniel Ionita <danielionita88@yahoo.com>
2020-05-26 18:45:16 -0500

    created firebase database, added post/get requests

--- React-Hooks/src/components/Ingredients/Ingredients.js
+++ React-Hooks/src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
@@ -7,25 +7,51 @@ import Search from './Search';
 const Ingredients = () => {
   const [ingredients, setIngredients] = useState([]);
 
-  const addIngredientHandler= ingredient => {
-    setIngredients(prevIngredients => [
-      ...prevIngredients, 
-      {id: Math.random().toString(), ...ingredient}
-    ]);
+  useEffect(() => {
+    fetch('https://react-hooks-25390.firebaseio.com/ingredients.json')
+      .then(resp => resp.json())
+      .then(respData => {
+        const loadedIngredients = [];
+        for (const key in respData) {
+          loadedIngredients.push({
+            id: key,
+            title: respData[key].title,
+            amount: respData[key].amount
+          });
+        }
+        setIngredients(loadedIngredients);
+      });
+  }, []);
+
+  const addIngredientHandler = ingredient => {
+    fetch('https://react-hooks-25390.firebaseio.com/ingredients.json', {
+      method: 'POST',
+      body: JSON.stringify(ingredient),
+      headers: {
+        'Content-type': 'application/json'
+      }
+    }).then(response => {
+      return response.json();
+    }).then(responseData => {
+      setIngredients(prevIngredients => [
+        ...prevIngredients,
+        { id: responseData.name, ...ingredient }
+      ]);
+    })
   };
 
-  const removeIngredientHandler= id => {
+  const removeIngredientHandler = id => {
     const newIngredients = ingredients.filter(ing => ing.id !== id)
     setIngredients(newIngredients)
   }
 
   return (
     <div className="App">
-      <IngredientForm onAddIngredient={addIngredientHandler}/>
+      <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
         <Search />
-        <IngredientList ingredients={ingredients} onRemoveItem={removeIngredientHandler}/>
+        <IngredientList ingredients={ingredients} onRemoveItem={removeIngredientHandler} />
       </section>
     </div>
   );
